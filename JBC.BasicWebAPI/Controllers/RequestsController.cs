0a3f55843653662deeaf98b839adae0497c4ea03
Jamie Cox <jamie.cox@stockport.gov.uk>
2016-04-06 16:39:33 +0100

    Initial commit

---
+++ JBC.BasicWebAPI/Controllers/RequestsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace JBC.BasicWebAPI
+{
+    [RoutePrefix("api/v1/requests")]
+    public class RequestsController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [Route("{id:int}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [Route("")]
+        public void Post([FromBody]string value)
+        {
+        }
+
+    }
+}
---
+++ JBC.BasicWebAPI/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using Owin;
+using Swashbuckle.Application;
+
+namespace JBC.BasicWebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+            httpConfiguration
+            .EnableSwagger(c => c.SingleApiVersion("v1", "JBC.BasicWebAPI"))
+            .EnableSwaggerUi(); 
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+    }
+}
