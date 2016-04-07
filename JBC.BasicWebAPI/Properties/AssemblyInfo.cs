91ae7079b9685846e9855899b2e9efd52f1cfa46
Jamie Cox <jamie.cox@stockport.gov.uk>
2016-04-07 10:04:54 +0100

    Tweaks

--- JBC.BasicWebAPI/Properties/AssemblyInfo.cs
+++ JBC.BasicWebAPI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Smbc.Api.Place")]
+[assembly: AssemblyTitle("JBC.BasicWebAPI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Smbc.Api.Place")]
+[assembly: AssemblyProduct("JBC.BasicWebAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
